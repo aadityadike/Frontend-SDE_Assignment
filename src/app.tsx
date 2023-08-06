a6fb2bedd704ee1a968b0df94f0d75111715df0c
aadityadike <adityadike123@gmail.com>
2023-08-06 20:03:50 +0530

    Important Funcionalities are done

--- src/app.tsx
+++ src/app.tsx
@@ -2,29 +2,29 @@ import { useState } from "react";
 import List from "./components/List";
 
 function App() {
-    const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState("");
 
-    const captureInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchValue(e.target.value);
-    }
+  const captureInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
 
-    return (
-        <div className="flex flex-col items-center justify-center gap-4 relative top-40">
-            {/* Search Box */}
-            <div className="flex justify-center items-center w-5/12 h-18">
-                <input
-                    className="border border-black rounded w-full h-10 px-2"
-                    type="text"
-                    placeholder="Search"
-                    value={searchValue}
-                    onChange={captureInput}
-                />
-            </div>
-            <div className="container border border-black rounded w-5/12 h-80 overflow-hidden overflow-y-auto px-5">
-                <List name={searchValue} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 relative top-36">
+      {/* Search Box */}
+      <div className="flex justify-center items-center w-5/12 h-18">
+        <input
+          className="border border-black rounded w-full h-10 px-2"
+          type="text"
+          placeholder="Search"
+          value={searchValue}
+          onChange={captureInput}
+        />
+      </div>
+      <div className="container rounded w-5/12 h-screen overflow-hidden overflow-y-auto px-5">
+        <List name={searchValue} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,73 +1,97 @@
 import { VscChevronDown } from "react-icons/vsc";
 import { VscChevronUp } from "react-icons/vsc";
+import { BsPencil } from "react-icons/bs";
+import { BsTrash } from "react-icons/bs";
 import CelebData from "../type.ts";
 import { useState } from "react";
 
 function User({ user }: { user: CelebData }) {
-    function calculateAge(dataOfBirth: string): number {
-        const now = new Date();
-        const birthDate = new Date(dataOfBirth);
-        let age = now.getFullYear() - birthDate.getFullYear();
-        const monthDiff = now.getMonth() - birthDate.getMonth();
+  const [open, setOpen] = useState(false);
 
-        if (
-            monthDiff < 0 ||
-            (monthDiff == 0 && now.getDate() < birthDate.getDate())
-        ) {
-            age--;
-        }
+  // Calculating Age
+  function calculateAge(dataOfBirth: string): number {
+    const now = new Date();
+    const birthDate = new Date(dataOfBirth);
+    let age = now.getFullYear() - birthDate.getFullYear();
+    const monthDiff = now.getMonth() - birthDate.getMonth();
 
-        return age;
+    if (
+      monthDiff < 0 ||
+      (monthDiff == 0 && now.getDate() < birthDate.getDate())
+    ) {
+      age--;
     }
 
-    const [open, setOpen] = useState(false);
-    return (
-        <div className="flex flex-col py-3 m-5 gap-9 border border-slate-950 rounded-md items-center">
-            <div className="flex items-center gap-4 w-full">
-                <div className="flex gap-5 items-center flex-1 pl-4">
-                    <img className="rounded-full w-14 h-14" src={user.picture} alt="Picture" />
-                    <h1 className="font-bold flex-1">
-                        {user.first} {user.last}
-                    </h1>
-                </div>
-                <button
-                    className="w-10 h-10 pr-12"
-                    onClick={() => {
-                        setOpen(!open);
-                    }}
-                >
-                    {!open ? (
-                        <VscChevronDown className="w-6 h-6" />
-                    ) : (
-                        <VscChevronUp className="w-6 h-6" />
-                    )}
-                </button>
-            </div>
-            {open && (
-                <>
-                    <div className="grid grid-cols-3 gap-6">
-                        <div>
-                            <h1 className="font-bold">Age</h1>
-                            <div>{calculateAge(user.dob)} Years</div>
-                        </div>
-                        <div>
-                            <h1 className="font-bold">Gender</h1>
-                            <div>{user.gender}</div>
-                        </div>
-                        <div>
-                            <h1 className="font-bold">Country</h1>
-                            <div>{user.country}</div>
-                        </div>
-                    </div>
-                    {/* description */}
-                    <div className="px-5">
-                        <h1 className="font-bold">Description</h1>
-                        <p>{user.description}</p>
-                    </div>
-                </>
+    return age;
+  }
 
-            )}
-        </div>);
+  // Handle Delete
+  function HandleDelete(): undefined {
+    if (window.confirm("Are you sure you want to delete the user?")) {
+    }
+  }
+
+  return (
+    <div className="flex flex-col py-3 m-5 gap-9 border border-slate-950 rounded-md items-center">
+      <div className="flex items-center gap-4 w-full">
+        <div className="flex gap-5 items-center flex-1 pl-4">
+          <img
+            className="rounded-full w-14 h-14"
+            src={user.picture}
+            alt="Picture"
+          />
+          <h1 className="font-bold flex-1">
+            {user.first} {user.last}
+          </h1>
+        </div>
+        <button
+          className="w-10 h-10 pr-12"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          {!open ? (
+            <VscChevronDown className="w-6 h-6" />
+          ) : (
+            <VscChevronUp className="w-6 h-6" />
+          )}
+        </button>
+      </div>
+      {open && (
+        <>
+          <div className="grid grid-cols-3 gap-6">
+            <div>
+              <h1 className="font-bold">Age</h1>
+              <div>{calculateAge(user.dob)} Years</div>
+            </div>
+            <div>
+              <h1 className="font-bold">Gender</h1>
+              <div>{user.gender}</div>
+            </div>
+            <div>
+              <h1 className="font-bold">Country</h1>
+              <div>{user.country}</div>
+            </div>
+          </div>
+          {/* description */}
+          <div className="px-5">
+            <h1 className="font-bold">Description</h1>
+            <p>{user.description}</p>
+          </div>
+
+          {/* Edit & Delete Button */}
+          <div className="flex left-44 relative gap-8">
+            <button onClick={() => HandleDelete()}>
+              <BsTrash className="w-7 h-7" style={{ color: "red" }} />
+            </button>
+            <button>
+              <BsPencil className="w-7 h-7" style={{ color: "#0096FF" }} />
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default User;
